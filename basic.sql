0411fa8b6b90aa34302876dc57d9f1e084829b4a
muskan <muskan8878h@gmail.com>
2024-08-01 20:44:00 +0530

    ANY command implementation

---
+++ basic.sql
@@ -0,0 +1,12 @@
+create table students (name varchar(20), age int, rollNo int);
+insert into students  values('hardik', 12, 1);
+insert into students  values('ankit', 14, 2);
+insert into students  values('anamika', 15, 3);
+select * from students;
+desc students;
+ALTER TABLE students Add(address varchar(30));
+ALTER TABLE students MODIFY address int;
+ALTER TABLE students RENAME COLUMN  address TO city;
+ALTER TABLE students RENAME TO stud;
+SELECT * FROM  stud;
+ALTER TABLE stud ADD PRIMARY KEY (rollNo);
