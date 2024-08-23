b795bb4079c921506481b655c1d558c12fedcec5
muskan <muskan8878h@gmail.com>
2024-08-23 13:26:55 +0530

    practice previous concept

--- table.sql
+++ table.sql
@@ -1,4 +1,4 @@
-create table employee(empid int,empname varchar(20),dept varchar(20),salary int);
+create table employee(empid int,empname varchar(20),dept varchar(20),salary int);    --create 
 insert into employee values(2,'arjun','btech',25000);
 insert into employee values(4,'situ','mba',20000); 
 insert into employee values(6,'manju','bba',40000);
@@ -6,7 +6,18 @@ insert into employee values(3,'varun','bba',15000);
 insert into employee values(5,'seema','pharmacy',30000);
 insert into employee values(7,'mohit','marine',45000);
 insert into employee values(1,'ram','cse',20000);
+insert into employee values(1,'sham','',20000);
 select * from employee;
+
+select empid, empname,dept,salary from employee where dept='bba' and salary<=40000;  --where clause
+select * from employee where dept is null;    --is null
+select * from employee where dept is not null;    --is not null
+insert into employee (salary) values(1000);     --insert specific value 
+update employee set empname='shruti' where salary=1000;    --update 
+delete from employee where empid=6;         --delete
+create view virtual as select dept, empname from employee; 
+select * from virtual;
+
 
 create table dept(dept varchar(20), empid int);
 insert into dept values('cse',1);
